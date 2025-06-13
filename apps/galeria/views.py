6958cda0d513e73a11cd938a5307de9cc0b2e2c7
ronaldodeschain <lotushall@gmail.com>
2025-06-13 11:39:25 -0300

    pausa para o almoço

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render,get_object_or_404,redirect
 from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
 from django.contrib import messages
 
 
@@ -37,7 +38,8 @@ def buscar(request):
     return render(request,"galeria/buscar.html",{"cards":fotografias})
 
 def nova_imagem(request):
-    return render(request,'galeria/nova_imagem.html')
+    form = FotografiaForms
+    return render(request,'galeria/nova_imagem.html', {'form':form})
 
 def editar_imagem(request):
     ...
